54de6b91cda0ce9dcc60794182b2be260693b9a4
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-06-14 16:57:09 +0530

    feat: style header components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,14 +4,17 @@ import { Link } from "gatsby"
 // Hooks
 import { useSiteConfigQuery } from "hooks/useSiteConfigQuery"
 
+// Styles
+import { Wrapper, Logo } from "./Header.styles"
+
 const Header = ({ siteTitle = `` }) => {
   const siteConfig = useSiteConfigQuery()
   return (
-    <div>
+    <Wrapper>
       <Link to="/">
-        <img src={siteConfig.logo.publicURL} alt={siteTitle} />
+        <Logo src={siteConfig.logo.publicURL} alt={siteTitle} />
       </Link>
-    </div>
+    </Wrapper>
   )
 }
 
