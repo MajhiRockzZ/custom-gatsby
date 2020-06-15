cd49d1c161375c05c24070606d68843080cb1845
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-06-15 17:53:20 +0530

    feat: style background image for hero component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 // Component
+import BGImage from "components/BGImage"
 // Hooks
 import { useHeroQuery } from "hooks/useHeroQuery"
 // Styles
@@ -16,14 +17,14 @@ const Hero = () => {
 
   return (
     <div>
-      <div>
+      <BGImage title="heroImage" fluid={heroImage.childImageSharp.fluid}>
         <div>
           <h2>{heroImageText}</h2>
           <Link to={heroImageBtnLink}>
             <button>{heroImageBtnText}</button>
           </Link>
         </div>
-      </div>
+      </BGImage>
     </div>
   )
 }
