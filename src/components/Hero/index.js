e8908218cba22647bf20a641c07ecaf4ca169bea
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-06-15 16:39:14 +0530

    feat: use hook to fetch hero content

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,16 +2,25 @@ import React from "react"
 import { Link } from "gatsby"
 // Component
 // Hooks
+import { useHeroQuery } from "hooks/useHeroQuery"
 // Styles
 
 const Hero = () => {
+  const {
+    heroImage,
+    heroImageText,
+    heroImageBtnText,
+    heroImageBtnLink,
+  } = useHeroQuery()
+  console.log(heroImage)
+
   return (
     <div>
       <div>
         <div>
-          <h2>Title</h2>
-          <Link to="/">
-            <button>Click me</button>
+          <h2>{heroImageText}</h2>
+          <Link to={heroImageBtnLink}>
+            <button>{heroImageBtnText}</button>
           </Link>
         </div>
       </div>
