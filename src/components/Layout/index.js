d02b3473ceef9b7bcebf6d70f5ce208f2f4743b5
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-06-16 07:11:44 +0530

    feat: implement light and dark mode

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useContext } from "react"
 import { ThemeProvider } from "styled-components"
 // Hooks
 import { useMetaDataQuery } from "hooks/useMetaDataQuery"
@@ -6,15 +6,17 @@ import { useMetaDataQuery } from "hooks/useMetaDataQuery"
 // Components
 import Header from "components/Header"
 
+// Context
+import { ModeContext } from "context/ModeProvider"
+
 // Styles
 import { GlobalStyles, lightTheme, darkTheme } from "styles/GlobalStyles"
 
 const Layout = ({ children }) => {
   const data = useMetaDataQuery()
-  console.log(data)
-
+  const [darkMode] = useContext(ModeContext)
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <GlobalStyles />
       <Header siteTitle={data.title} />
       {children}
